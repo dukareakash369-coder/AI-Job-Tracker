54ace82f5b889141e5073d781bab666357e688d0
AKASH ARUN DUKARE <dukareakash369@gmail.com>
2026-09-07 01:38:19 +0530

    Update job_tracker.py

--- job_tracker.py
+++ job_tracker.py
@@ -1,4 +1,4 @@
-import os
+mport os
 import json
 import re
 from datetime import date
@@ -36,8 +36,7 @@ if not GOOGLE_JSON:
 
 # Google Sheets authorization
 SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
+    "https://www.googleapis.com/auth/spreadsheets"
 ]
 
 try:
@@ -49,6 +48,7 @@ try:
     )
 
     gc = gspread.authorize(credentials)
+    print("Google service account:", service_account_info.get("client_email"))
 
 except Exception as error:
     print("Google authentication error:", error)
